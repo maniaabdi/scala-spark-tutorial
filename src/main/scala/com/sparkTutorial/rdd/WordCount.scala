c5383e660f97fd37fe85d9e2e0e66363e615c404
mania <mania.abdi287@gmail.com>
2018-08-02 03:27:40 +0000

    update word count

--- src/main/scala/com/sparkTutorial/rdd/WordCount.scala
+++ src/main/scala/com/sparkTutorial/rdd/WordCount.scala
@@ -5,6 +5,11 @@ import org.apache.log4j.Logger
 import org.apache.spark.SparkConf
 import org.apache.spark._
 
+import java.util.Timer
+import org.apache.spark.util
+import java.lang.System;
+
+
 object WordCount {
 
   def main(args: Array[String]) {
@@ -12,11 +17,28 @@ object WordCount {
     Logger.getLogger("org").setLevel(Level.ERROR)
     val conf = new SparkConf().setAppName("wordCounts").setMaster("local[3]")
     val sc = new SparkContext(conf)
+    val timeout = args(0).toInt // mania
 
-    val lines = sc.textFile("in/word_count.text")
-    val words = lines.flatMap(line => line.split(" "))
+    // mania
+    val t = new java.util.Timer()
+    val task = new java.util.TimerTask {
+        def run() {
+            println("YYYOOOOOOOOOOOOOO")
+            //sc.session.stop();
+             this.cancel()
+            sc.cancelAllJobs()
+            sc.stop()
+            System.exit(0)
+        }
+    } 
+    t.schedule(task, timeout*1000, timeout*1000)
 
-    val wordCounts = words.countByValue()
-    for ((word, count) <- wordCounts) println(word + " : " + count)
+    while(true) {
+      val lines = sc.textFile("in/word_count.text")
+      val words = lines.flatMap(line => line.split(" "))
+
+      val wordCounts = words.countByValue()
+      for ((word, count) <- wordCounts) println(word + " : " + count)
+    }
   }
 }
